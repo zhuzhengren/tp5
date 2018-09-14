3a3c2625393833bbae8622bfa6e450ee429f9eec
zhuzhengren <zhuzr@yilan.tv>
2018-09-15 01:51:50 +0800

    收藏与点赞

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -7,12 +7,11 @@ use app\common\model\ArticleCate;
 use app\common\model\Article;
 use think\facade\Request;
 use think\Db;
+use think\facade\Session;
 
 class Index extends Base {
 
     public function index() {
-
-
         //全局查询条件
         $map = [];
 
@@ -25,8 +24,12 @@ class Index extends Base {
         if (!empty($keywords)) {
             //条件2
             $map[] = ['title', 'like', '%' . $keywords . '%'];
+            //点击分页查询按钮时额外的参数，防止点击搜索结果第二页参数消失的问题
+            $pagQuery=['keywords'=>$keywords];
+        }else{
+            $pagQuery=[];
         }
-
+        
         //分类信息显示
         $cateId = Request::param('cate_id');
         //如果存在分类ID
@@ -39,14 +42,22 @@ class Index extends Base {
             $artList = Db::table('zh_article')
                     ->where($map)
                     ->order('create_time', 'desc')
-                    ->paginate(3);
+                    ->paginate(3,false,[
+                        'query'=>$pagQuery,
+                        'type'=>'bootstrap',
+                        'var_page'=>'artList',
+                    ]);
 
             $this->view->assign('cateName', $res->name);
         } else {
             $artList = Db::table('zh_article')
                     ->where($map)
                     ->order('create_time', 'desc')
-                    ->paginate(3);
+                    ->paginate(3,false,[
+                        'query'=>$pagQuery,
+                        'type'=>'bootstrap',
+                        'var_page'=>'artList',
+                    ]);
             $this->view->assign('cateName', "全部文章");
         }
         $this->view->assign('empty', '<h3>没有文章</h3>');
@@ -125,11 +136,104 @@ class Index extends Base {
         $art = Article::get(function ($query)use ($artId) {
                     $query->where('id', '=', $artId)->setInc('pv');
                 });
-        if(!is_null($art)){
-            $this->view->assign('art',$art);
+        if (!is_null($art)) {
+            $this->view->assign('art', $art);
         }
         $this->view->assign('title', '详情页');
+
+        //同步收藏状态和点赞状态
+        //用户未登陆默认显示未收藏和点赞
+        if (!Session::has('user_id')) {
+            $this->view->assign('like', 0);
+            $this->view->assign('fav', 0);
+        } else {
+            //数据库查询条件
+            $map[] = ['user_id', '=', Session::get('user_id')];
+            $map[] = ['art_id', '=', $artId];
+            $fav = Db::table('zh_user_collect')->where($map)->find();
+            //判断是否收藏
+            if(is_null($fav)){
+                $this->view->assign('fav', 0);
+            }else{
+                $this->view->assign('fav', 1);
+            }
+            //判断是否点赞
+            $like = Db::table('zh_user_like')->where($map)->find();
+            if(is_null($like)){
+                $this->view->assign('like', 0);
+            }else{
+                $this->view->assign('like', 1);
+            }
+        }
+
+
         return $this->view->fetch('detail');
+    }
+
+    //收藏
+    public function fav() {
+        //请求方式必须正确
+        if (!Request::isAjax()) {
+            return ['status' => -1, 'message' => '请求方式错误'];
+        }
+        $data = Request::param();
+        //用户未登陆不允许操作
+        //halt($data);
+        if (empty($data['session_id'])) {
+            // halt($data);
+            return ['status' => -2, 'message' => '用户未登陆'];
+        }
+        //数据库查询条件
+        $map[] = ['user_id', '=', $data['session_id']];
+        $map[] = ['art_id', '=', $data['art_id']];
+        //查询是否存在收藏记录
+        $fav = Db::table('zh_user_collect')->where($map)->find();
+        if (is_null($fav)) {
+            //未收藏 点击收藏
+            Db::table('zh_user_collect')->data([
+                'user_id' => $data['session_id'],
+                'art_id' => $data['art_id'],
+            ])->insert();
+            //halt($data);
+            return ['status' => 1, 'message' => '收藏成功'];
+        } else {
+            //已收藏 取消收藏
+            Db::table('zh_user_collect')->where($map)->delete();
+            return ['status' => 0, 'message' => '已取消收藏'];
+        }
+    }
+
+    //点赞
+    public function like() {
+        //请求方式必须正确
+        if (!Request::isAjax()) {
+            return ['status' => -1, 'message' => '请求方式错误'];
+        }
+        $data = Request::param();
+        //用户未登陆不允许操作
+        //halt($data);
+        if (empty($data['session_id'])) {
+            // halt($data);
+            return ['status' => -2, 'message' => '用户未登陆'];
+        }
+        //数据库查询条件
+        $map[] = ['user_id', '=', $data['session_id']];
+        $map[] = ['art_id', '=', $data['art_id']];
+        //查询是否存在收藏记录
+        $fav = Db::table('zh_user_like')->where($map)->find();
+        if (is_null($fav)) {
+            //未收藏 点击收藏
+            Db::table('zh_user_like')->data([
+                'user_id' => $data['session_id'],
+                'art_id' => $data['art_id'],
+            ])->insert();
+            //halt($data);
+            return ['status' => 1, 'message' => '点赞成功'];
+        } else {
+            //已收藏 取消收藏
+            Db::table('zh_user_like')->where($map)->delete();
+            return ['status' => 0, 'message' => '已取消点赞'];
+        }
     }
 
 }
